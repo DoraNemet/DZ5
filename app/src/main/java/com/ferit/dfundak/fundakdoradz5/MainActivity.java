7c9e52cf458793889ca0bed8b28c5b3c9338013c
Dora Fundak <dora.fundak@gmail.com>
2017-04-22 17:52:53 +0200

    First Commit

---
+++ app/src/main/java/com/ferit/dfundak/fundakdoradz5/MainActivity.java
@@ -0,0 +1,309 @@
+package com.ferit.dfundak.fundakdoradz5;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Criteria;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+import static android.media.AudioManager.AUDIOFOCUS_LOSS_TRANSIENT;
+import static android.media.AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private static final int REQUEST_LOCATION_PERMISSION = 10;
+    TextView tvLocationText;
+    Button takePicture;
+    LocationListener mLocationListener;
+    LocationManager mLocationManager;
+    GoogleMap mGoogleMap;
+    MapFragment mMapFragment;
+    private GoogleMap.OnMapClickListener mCustomOnMapClickListener;
+
+    private MediaPlayer mediaPlayer;
+    private AudioManager mAudioManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mAudioManager = (AudioManager) this.getSystemService(Context.AUDIO_SERVICE);
+
+        this.tvLocationText = (TextView) this.findViewById(R.id.tvLocationText);
+        this.mLocationManager = (LocationManager) this.getSystemService(LOCATION_SERVICE);
+        this.mLocationListener = new SimpleLocationListener();
+        this.takePicture = (Button) findViewById(R.id.btnTakePicture);
+
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            takePicture.setEnabled(false);
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.CAMERA, android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, 0);
+        }
+
+        this.initialize();
+
+        this.takePicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                takePicture(v);
+            }
+        });
+    }
+
+    private AudioManager.OnAudioFocusChangeListener afChangeListener = new AudioManager.OnAudioFocusChangeListener() {
+        @Override
+        public void onAudioFocusChange(int focusChange) {
+            if (focusChange == AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK || focusChange == AUDIOFOCUS_LOSS_TRANSIENT) {
+                mediaPlayer.pause();
+                mediaPlayer.seekTo(0);
+            }  else if (focusChange == AudioManager.AUDIOFOCUS_LOSS ) {
+                releseMediaPlayer();
+            } else if (focusChange == AudioManager.AUDIOFOCUS_GAIN) {
+                mediaPlayer.start();
+            }
+        }
+    };
+
+    private MediaPlayer.OnCompletionListener mCompletionListener = new MediaPlayer.OnCompletionListener() {
+        @Override
+        public void onCompletion(MediaPlayer mediaPlayer) {
+            releseMediaPlayer();
+        }
+    };
+
+    private void releseMediaPlayer() {
+        if (mediaPlayer != null) {
+            mediaPlayer.release();
+            mediaPlayer = null;
+            mAudioManager.abandonAudioFocus(afChangeListener);
+        }
+    }
+
+    private void initialize() {
+        this.mMapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.fGoogleMap);
+        this.mMapFragment.getMapAsync(this);
+        this.mCustomOnMapClickListener = new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                MarkerOptions newMarkerOptions = new MarkerOptions();
+                newMarkerOptions.icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher));
+                newMarkerOptions.title("Here");
+                newMarkerOptions.position(latLng);
+                mGoogleMap.addMarker(newMarkerOptions);
+
+                releseMediaPlayer();
+
+                int result = mAudioManager.requestAudioFocus(afChangeListener, AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
+
+                if (result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
+
+                    mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.short_sound);
+                    mediaPlayer.start();
+                    mediaPlayer.setOnCompletionListener(mCompletionListener);
+                }
+
+            }
+        };
+    }
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        this.mGoogleMap = googleMap;
+        UiSettings uiSettings = this.mGoogleMap.getUiSettings();
+        uiSettings.setZoomControlsEnabled(true);
+        uiSettings.setMyLocationButtonEnabled(true);
+        uiSettings.setZoomGesturesEnabled(true);
+        this.mGoogleMap.setOnMapClickListener(this.mCustomOnMapClickListener);
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) !=PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        this.mGoogleMap.setMyLocationEnabled(true);
+    }
+
+    private void takePicture(View v) {
+        Toast.makeText(this, "cheese", Toast.LENGTH_SHORT).show();
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (hasLocationPermission() == false) {
+            requestPermission();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (this.hasLocationPermission()) {
+            startTracking();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        releseMediaPlayer();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        stopTracking();
+    }
+
+    //starts GPS tracking
+    private void startTracking() {
+        Criteria kriterij = new Criteria();
+        kriterij.setAccuracy(Criteria.ACCURACY_FINE);
+        String locationProvider = this.mLocationManager.getBestProvider(kriterij, true);
+        long minTime = 2000;
+        float minDistance = 10;
+        this.mLocationManager.requestLocationUpdates(locationProvider, minTime, minDistance,
+                this.mLocationListener);
+    }
+
+    //stops GPS tracking
+    private void stopTracking() {
+        this.mLocationManager.removeUpdates(this.mLocationListener);
+    }
+
+    //check if permission is granted
+    private boolean hasLocationPermission() {
+        String LocationPermission = android.Manifest.permission.ACCESS_FINE_LOCATION;
+        int premission = ContextCompat.checkSelfPermission(this, LocationPermission);
+        if (premission == PackageManager.PERMISSION_GRANTED) {
+            return true;
+        }
+        return false;
+    }
+
+    //requesting permission for location
+    private void requestPermission() {
+        String[] permissions = new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION};
+        ActivityCompat.requestPermissions(MainActivity.this,
+                permissions, REQUEST_LOCATION_PERMISSION);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_LOCATION_PERMISSION:
+                if (grantResults.length > 0) {
+                    if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                        Log.d("MainActivity", "Permission granted");
+                        takePicture.setEnabled(true);
+                    } else {
+                        Log.d("MainActivity", "Permission denyed");
+                        askForPermission();
+                    }
+                }
+        }
+    }
+
+    private void askForPermission() {
+        boolean shouldExplain = ActivityCompat.shouldShowRequestPermissionRationale(
+                MainActivity.this, android.Manifest.permission.ACCESS_FINE_LOCATION);
+        if (shouldExplain) {
+            this.displayDialog();
+        } else {
+            tvLocationText.setText("Application can't work without GPS permission");
+        }
+    }
+
+    //ask user for permission
+    private void displayDialog() {
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+        dialogBuilder.setTitle("Location permission")
+                .setMessage("App needs your permission to display your location")
+                .setNegativeButton("Dismiss", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                })
+                .setPositiveButton("Allow", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        requestPermission();
+                        dialog.cancel();
+                    }
+                })
+                .show();
+    }
+
+    //show location on screen
+    private void DisplayLocation(Location location) {
+        String message = "Lat: " + location.getLatitude() + "\nLon:" + location.getLongitude() + "\n";
+        tvLocationText.setText(message);
+
+        if (Geocoder.isPresent()) {
+            Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+            try {
+                List<Address> nearByAddresses = geocoder.getFromLocation(
+                        location.getLatitude(), location.getLongitude(), 1);
+                if (nearByAddresses.size() > 0) {
+                    StringBuilder stringBuilder = new StringBuilder();
+                    Address nearestAddress = nearByAddresses.get(0);
+                    stringBuilder.append(nearestAddress.getAddressLine(0)).append("\n")
+                            .append(nearestAddress.getLocality()).append("\n")
+                            .append(nearestAddress.getCountryName());
+                    tvLocationText.append(stringBuilder.toString());
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private class SimpleLocationListener implements LocationListener {
+        @Override
+        public void onLocationChanged(Location location) {
+            DisplayLocation(location);
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+        }
+    }
+
+
+}
